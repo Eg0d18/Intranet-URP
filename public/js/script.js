47089f159112cad7735e29d5ba83cc172cc93397
Eg0d18 <diegodiazbazan.04@gmail.com>
2024-11-25 21:54:45 -0500

    feat: add login and home view

---
+++ public/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", function() {
+    var iframe = document.getElementById("myFrame");
+    iframe.src = "http://127.0.0.1:5500/index.html";
+});
